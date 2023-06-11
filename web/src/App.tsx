054c746dde4160a296a84b9c5b92c44dcd232286
kangxinzhi <820073312@qq.com>
2023-06-11 20:28:58 +0800

    feat: 完善配置

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useLayoutEffect } from 'react'
 import './App.css'
 import { World, Cube, Model, OrbitCamera, Skybox, useLoop, Editor } from 'lingo3d-react'
 import SpeechRecognitionSingleton from './utils/SpeechRecognitionSingleton'
 import SpeechSynthesisSingleton from './utils/SpeechSynthesisSingleton'
+import LoadingSpinner from './components/LoadingSpinner'
+import { debounce } from './utils'
 
 const recognition = SpeechRecognitionSingleton.getInstance()
 const synth = SpeechSynthesisSingleton.getInstance()
@@ -11,38 +13,54 @@ const synth = SpeechSynthesisSingleton.getInstance()
 function App() {
   const [msg, setMsg] = useState('')
   const [outMessage, setOutputMessage] = useState('')
-
   const [tempMsg, setTempMsg] = useState('')
-
   const [openMic, setOpenMic] = useState(false)
-  let final_transcript = "";
+  const [isLoading, setIsLoading] = useState(false)
+  const [pose, setPose] = useState('idle')
+  let final_transcript = ""
+
+  const onSpeechEnd = () => {
+    setPose('idle')
+    console.log('Speech completed')
+    // 在这里执行后续操作
+  }
 
   const handleSend = () => {
-    synth.speak('谷歌展出了他们的智能语音助理，其人声的拟真度把所有观众惊呆了。')
-    // const temp = msg
-    // // 将api_url替换为你的API接口地址
-    // const api_url = 'http://127.0.0.1:8002/chat'
+    if (isLoading || !msg) return
 
-    // const testData = { topic: temp }
+    setIsLoading(true)
+    // setPose('talking')
+    // synth.speak('谷歌展出了他们的智能语音助理', onSpeechEnd)
 
-    // // 发送POST请求
-    // fetch(api_url, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify(testData),
-    // })
-    //   .then(response => response.json())
-    //   .then((data) => {
-    //     // 处理响应数据
-    //     console.log(data)
-    //     synth.speak(JSON.stringify(data.text))
-    //     setOutputMessage(JSON.stringify(data.text))
-    //   })
-    //   .catch((error) => {
-    //     console.error(error)
-    //   })
+    // 将api_url替换为你的API接口地址
+    const api_url = 'https://ai.kidkid.tech/api/get-answer'
+
+    // synth.speak('请耐心等候', onSpeechEnd)
+    // 发送POST请求
+    fetch(api_url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ question: msg }),
+    })
+      .then(response => response.json())
+      .then((data) => {
+        // 处理响应数据
+        console.log(data)
+        setPose('talking')
+        // synth.speak(JSON.stringify(data.text),onSpeechEnd)
+        synth.speak('谷歌展出了他们的智能语音助理', onSpeechEnd)
+        setOutputMessage(JSON.stringify(data.text))
+        setIsLoading(false)
+
+      })
+      .catch((error) => {
+        console.error(error)
+        setPose('talking')
+        synth.speak('网络有些问题，请稍后重试', onSpeechEnd)
+        setIsLoading(false)
+      })
   }
 
   const handlePressStart = async () => {
@@ -51,11 +69,13 @@ function App() {
     setOpenMic(true)
   };
 
-  const handlePressEnd = async () => {
+  const handlePressEnd = () => {
+    console.log('handlePressEnd')
     if (tempMsg) {
       setMsg(msg + tempMsg)
     }
     setTempMsg('')
+    handleSend()
     setOpenMic(false)
   };
 
@@ -88,54 +108,64 @@ function App() {
   useEffect(() => {
     if (!recognition)
       return
-    if (openMic)
+    if (openMic){
       recognition.start()
-    else
+    }else{
       recognition.abort()
+    }
+      
   }, [openMic])
 
   return (
     <div>
       <World skybox={'sky1.jpg'}>
         <Model
-          src="Boy.fbx"
-          scale={5}
-          toon={true}
-          animations={{ idle: 'Idle.fbx', talking: 'Talking.fbx', waving: 'Waving.fbx' }}
-          animation={"talking"}
+          src="Girl.fbx"
+          scale={8}
+          animations={{ idle: 'Idle.fbx', talking: 'Talking.fbx', waving: 'Waving.fbx',asking: 'Asking.fbx' }}
+          animation={pose}
           x={-300}
-          y={5}
+          y={-100}
           z={100}
-          rotationY={20}
+          rotationY={50}
         />
+        {/* <OrbitCamera  
+          active
+          // rotationY={320} 
+          // rotationX={10} 
+          // rotationZ={-20} 
+        /> */}
         {/* <Editor/> */}
       </World>
 
       <div className=" w-[30vw] h-full absolute right-0 top-0 bg-black text-white bg-opacity-30 ">
-        <span>{msg}{tempMsg}</span>
-        <hr/>
-        <span>
-          {outMessage}
-        </span>
-        <div className="box-border absolute bottom-0 w-full h-[300px]  flex items-center justify-center">
-          <div
-            className="w-16 h-16 flex items-center justify-center bg-green-400 rounded-full cursor-pointer"
-            onTouchStart={handlePressStart}
-            onTouchEnd={handlePressEnd}
-            onMouseDown={handlePressStart}
-            onMouseUp={handlePressEnd}
-            onMouseLeave={handlePressEnd}
-          >
-            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="28496" width="2.5rem" height="2.5rem"><path d="M841.142857 402.285714v73.142857c0 169.142857-128 308.553143-292.571428 326.838858V877.714286h146.285714c20.004571 0 36.571429 16.566857 36.571428 36.571428s-16.566857 36.571429-36.571428 36.571429H329.142857c-20.004571 0-36.571429-16.566857-36.571428-36.571429s16.566857-36.571429 36.571428-36.571428h146.285714v-75.446857c-164.571429-18.285714-292.571429-157.696-292.571428-326.838858v-73.142857c0-20.004571 16.566857-36.571429 36.571428-36.571428s36.571429 16.566857 36.571429 36.571428v73.142857c0 141.129143 114.870857 256 256 256s256-114.870857 256-256v-73.142857c0-20.004571 16.566857-36.571429 36.571429-36.571428s36.571429 16.566857 36.571428 36.571428z m-146.285714-219.428571v292.571428c0 100.571429-82.285714 182.857143-182.857143 182.857143s-182.857143-82.285714-182.857143-182.857143V182.857143c0-100.571429 82.285714-182.857143 182.857143-182.857143s182.857143 82.285714 182.857143 182.857143z" fill="#ffffff" p-id="28497"></path></svg>
+        {openMic && (
+          <span className="absolute left-[50%] top-[50%] flex items-center justify-center">
+            <div className="animate-ping rounded-full bg-green-500 w-12 h-12 absolute -left-6 -top-6"></div>
+            <div className="rounded-full bg-green-400 w-12 h-12 absolute -left-6 -top-6"></div>
+          </span>
+        )}
+        <div className="p-4">
+          <div className="flex items-start">
+            <span className="mr-2 font-bold">问题：{msg}{tempMsg}</span>
           </div>
-          <span className='w-[50px]'/>
-          <div
-            className="w-16 h-16 flex items-center justify-center bg-blue-400 rounded-full cursor-pointer"
-            onClick={handleSend}
-          >
-            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3059" width="2rem" height="2rem"><path d="M0 0l1024 512L0 1024V0z m0 409.6v204.8l512-102.4L0 409.6z" p-id="3060" fill="#ffffff"></path></svg>
-          </div>
+          <hr className="my-2" />
+          {isLoading && (<LoadingSpinner />)}
+          <div className="whitespace-pre-wrap">{outMessage}</div>
         </div>
+        {!isLoading && (
+          <div className="box-border absolute bottom-0 w-full h-[300px]  flex items-center justify-center">
+            <div
+              className="w-16 h-16 flex items-center justify-center bg-green-400 rounded-full cursor-pointer"
+              onTouchStart={handlePressStart}
+              onTouchEnd={handlePressEnd}
+              onMouseDown={handlePressStart}
+              onMouseUp={handlePressEnd}
+            >
+              <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="28496" width="2.5rem" height="2.5rem"><path d="M841.142857 402.285714v73.142857c0 169.142857-128 308.553143-292.571428 326.838858V877.714286h146.285714c20.004571 0 36.571429 16.566857 36.571428 36.571428s-16.566857 36.571429-36.571428 36.571429H329.142857c-20.004571 0-36.571429-16.566857-36.571428-36.571429s16.566857-36.571429 36.571428-36.571428h146.285714v-75.446857c-164.571429-18.285714-292.571429-157.696-292.571428-326.838858v-73.142857c0-20.004571 16.566857-36.571429 36.571428-36.571428s36.571429 16.566857 36.571429 36.571428v73.142857c0 141.129143 114.870857 256 256 256s256-114.870857 256-256v-73.142857c0-20.004571 16.566857-36.571429 36.571429-36.571428s36.571429 16.566857 36.571428 36.571428z m-146.285714-219.428571v292.571428c0 100.571429-82.285714 182.857143-182.857143 182.857143s-182.857143-82.285714-182.857143-182.857143V182.857143c0-100.571429 82.285714-182.857143 182.857143-182.857143s182.857143 82.285714 182.857143 182.857143z" fill="#ffffff" p-id="28497"></path></svg>
+            </div>
+          </div>
+        )}
       </div>
     </div>
 
